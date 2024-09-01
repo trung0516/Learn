b17f4fc1304b94832127c7698a62595ee69b8112
trung0516 <146793357+trung0516@users.noreply.github.com>
2024-09-01 14:46:51 +0700

    Add files via upload

---
+++ khaibaolop.java
@@ -0,0 +1,12 @@
+package LearnCode;
+import java.util.Date;
+public class khaibaolop{
+	public class NhanVien {
+		private String maNhanVien;
+		private String hoTen;
+		private Date ngaySinh;
+		private String diaChi;
+		private double heSoLuong;
+		private double luongCoBan;
+	}
+}
